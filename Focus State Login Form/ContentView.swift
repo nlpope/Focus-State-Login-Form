dc394b4b60c3113aa268073891e5fa6c9b93c0b6
Noah Pope <npope@tutanota.com>
2023-09-05 17:14:33 -0400

    Initial Commit

---
+++ Focus State Login Form/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Focus State Login Form
+//
+//  Created by Noah Pope on 9/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Focus State Login Form/Focus_State_Login_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_State_Login_FormApp.swift
+//  Focus State Login Form
+//
+//  Created by Noah Pope on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Focus_State_Login_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
